098707d63572c8e9d89d4e953b9124de86fe9c99
Damien Wilson <damien@absurd.engineering>
2025-06-15 12:21:04 -0700

    CLI interface to CSP parser.

---
+++ crates/cli/src/main.rs
@@ -0,0 +1,59 @@
+use clap::Parser;
+use parser::{ParseError, Policy};
+
+#[derive(Parser)]
+struct Args {
+    #[arg(short, long)]
+    policy: String,
+}
+
+fn main() -> Result<(), String> {
+    let args = Args::parse();
+    let policy = Policy::parse(&args.policy);
+
+    match policy {
+        Ok(policy) => {
+            println!("{:?}", policy);
+            Ok(())
+        },
+        Err(e) => {
+            println!("{}", explain_error(args.policy, &e));
+            Err("Unable to parse policy".to_string())
+        },
+    }
+}
+
+// Given a parse error, print the relevant line of the policy and highlight the character that caused the error.
+// Note that errors only contain the position of the error, not the line number.
+// We need to parse the policy to get the line number.
+fn explain_error(serialized_policy: String, error: &ParseError) -> String {
+    let mut output = String::new();
+
+    output.push_str(&error.to_string());
+    output.push_str(":\n\n");
+
+    output.push_str(&serialized_policy);
+    output.push_str("\n");
+
+    output.push_str(&" ".repeat(error.position()));
+    output.push_str("^\n");
+
+    output
+}
+
+// tests
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_print_error() {
+        let test_policy = "script-src 'self'; foo bar";
+        let error = Policy::parse(test_policy)
+            .expect_err("Valid policy in text that expects an error");
+
+        let output = explain_error(test_policy.to_string(), &error);
+
+        assert_eq!(output, "script-src 'self'; foo bar\n                   ^\nInvalid directive 'foo' at position 19\n");
+    }
+}
